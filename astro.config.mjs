f0f2100cbbf391d09dba20a857739d432b787c56
Mikhail Njorba <mnyorba@gmail.com>
2025-08-21 19:43:53 +0300

    [TASK] > Use settingS from const.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,14 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import { config } from './src/consts';
+
 // https://astro.build/config
 export default defineConfig({
+  site: config.url,
+  base: config.base,
+  trailingSlash: "always",
   vite: { plugins: [tailwindcss()], },
+  compressHTML: false,
   integrations: [mdx(), sitemap()],
 });
