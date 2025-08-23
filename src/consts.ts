b74e4f6c9a180062e98dd7e3ab32581d5fc8cca2
Mikhail Njorba <mnyorba@gmail.com>
2025-08-23 10:23:15 +0300

    feat: add support for base

--- src/consts.ts
+++ src/consts.ts
@@ -16,7 +16,7 @@ export const metadata = {
 // Site configuration
 export const config = {
   url: "https://example.com", // Your site's deploy URL. It must start with `https://` or `http://`.
-  base: "", // The base path of your site in your deploy URL. e.g. "/blog".
+  base: "/", // The base path of your site in your deploy URL. e.g. "/blog".
   postsPerPage: 10, // Number of posts per page.
 };
 
